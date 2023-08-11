556fbb5bdf5372eee1ad9fa8976b6a5478cda3b0
Kangilesh <f.peretto.89@gmail.com>
2023-08-10 21:32:58 -0300

    Fetch de pokemons completo y ficha individual básica

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,8 +1,60 @@
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
+import PokeballImg from '../assets/pokeball.png'
+import Footer from '../components/Footer';
+import styles from "./pokemons.module.css";
+import { useState, useEffect } from 'react';
+import { PokemonDetails } from '../types/types';
+import LoadingScreen from '../components/LoadingScreen';
+import { waitFor } from '../utils/utils';
+import { fetchPokemon } from "../api/fetchPokemon";
+
 
 const Pokemon = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [pokemon, setPokemon] = useState<PokemonDetails>()
     const { name } = useParams();
-    return <div>{name}</div>
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getPokemon() {
+            setIsLoading(true);
+            await waitFor (500);
+            const fetchedPokemon = await fetchPokemon(name as string);
+            setPokemon(fetchedPokemon)
+            setIsLoading(false);
+        }
+        getPokemon();
+    },[name])
+
+    if (isLoading || !pokemon) return <LoadingScreen />;
+
+    return (
+        <>
+            <button className={styles.pokeballButton} onClick={() => navigate(-1)}>
+                <img className={styles.pokeballImg} src={PokeballImg} alt="Pokeball" /> Go back
+            </button>
+            <div className={styles.pokemon}>
+                <main className={styles.pokemonInfo}>
+                    <div 
+                        className={styles.pokemonTitle}>
+                        {(name?.toUpperCase())}
+                    </div>
+                    <div>Nr {pokemon?.id}</div>
+                    <div>
+                        <img
+                            className={styles.pokemonInfoImg}
+                            src={pokemon?.imgSrc}
+                            alt={pokemon?.name} 
+                        />
+                    </div>
+                    <div>HP: {pokemon?.hp}</div>
+                    <div>Attack: {pokemon?.attack}</div>
+                    <div>Defense: {pokemon?.defense}</div>
+                </main>
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Pokemon;
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -6,25 +6,37 @@ import { Link } from 'react-router-dom';
 import { fetchPokemons } from "../api/fetchPokemons";
 import styles from "./pokemons.module.css"
 import { Pokemon } from "../types/types";
+import LoadingScreen from "../components/LoadingScreen";
 
 const Pokemons = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const [query, setQuery] = useState("");
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
     useEffect(() => {
         const fetchAllPokemons = async () => {
+            setIsLoading(true);
             const allPokemons = await fetchPokemons();
             setPokemons(allPokemons);
+            setIsLoading(false);
         };
     fetchAllPokemons();
     }, []);
     
+    if (isLoading || !pokemons) {
+        return <LoadingScreen />;
+    }
+    
+    const filteredPokemons = pokemons?.filter((pokemons => {
+        return pokemons.name.toLowerCase().match(query.toLowerCase());
+    }));
+
     return ( // fragment
         <> 
             <Header query={query} setQuery={setQuery} />
             <main>
                 <nav className={styles.nav}>
-                    {pokemons?.map((pokemon) => (
+                    {filteredPokemons?.map((pokemon) => (
                         <Link 
                             key={pokemon.id} 
                             className={styles.listItem} 
